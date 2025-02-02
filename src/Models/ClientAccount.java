a631b527621e5010d97c0b6850d54062be47c0eb
Andrew Muhlynin <rx.081@hotmail.com>
2025-02-02 09:57:21 +0500

    tests for task 3 is completed

--- src/Models/ClientAccount.java
+++ src/Models/ClientAccount.java
@@ -5,40 +5,19 @@ public class ClientAccount {
     private String name;
     private double balance;
 
-
-    public long getId() {
-        return id;
-    }
-    public void setId(long id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
-    }
-    public void setName(String name) {
-        this.name = name;
     }
 
     public double getBalance() {
         return balance;
     }
-    public void setBalance(double balance) {
-        this.balance = balance;
-    }
 
-
-    public ClientAccount() {}
+    public long getId() { return id; }
 
     public ClientAccount(long id, String name, double balance) {
         this.id = id;
         this.name = name;
         this.balance = balance;
-    }
-
-    public ClientAccount(ClientAccount clientAccount) {
-        id = clientAccount.id;
-        name = clientAccount.name;
-        balance = clientAccount.balance;
     }
 }
--- src/Tests/TransactionServices/TransactionProcessorTests.java
+++ src/Tests/TransactionServices/TransactionProcessorTests.java
@@ -1,4 +1,117 @@
 package Tests.TransactionServices;
 
+import AccountServices.Abstractions.BankAccount;
+import AccountServices.CreditAccount;
+import AccountServices.DebitAccount;
+import AccountServices.SavingsAccount;
+import Models.ClientAccount;
+import TransactionServices.TransactionProcessor;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class TransactionProcessorTests {
+
+    private double smallAmountValue = 995;
+    private double bigAmountValue = 6100;
+    private double midAmountValue = 3000;
+    private double creditLimit = -5000;
+
+    private TransactionProcessor transactionProcessor;
+
+    private List<BankAccount> accounts;
+
+    @BeforeEach
+    public void setup() {
+
+        transactionProcessor = new TransactionProcessor();
+        accounts = getAccountCollection();
+    }
+
+    @Test
+    public void processTransactionWithdrawDebitAndSavingsFailedCreditSuccess() {
+
+        transactionProcessor.processTransaction(accounts, midAmountValue);
+
+        accounts.forEach(account -> {
+
+            double startBalance = 1000;
+            double expectedBalance = startBalance;
+
+            if(account instanceof CreditAccount)
+            {
+                double transactionFee = 0.01;
+                double fee = midAmountValue * transactionFee;
+
+                expectedBalance = startBalance - midAmountValue - fee;
+
+                assertEquals(expectedBalance, account.getBalance());
+                assertTrue(account.getBalance() >= creditLimit);
+            }
+
+            else {
+                assertEquals(startBalance, account.getBalance());
+                assertTrue(account.getBalance() >= 0);
+            }
+        });
+    }
+
+    @Test
+    public void processTransactionWithdrawSuccessAll() {
+
+        transactionProcessor.processTransaction(accounts, smallAmountValue);
+
+        accounts.forEach(account -> {
+
+            double startBalance = 1000;
+            double expectedBalance = startBalance;
+
+            if(account instanceof CreditAccount)
+            {
+                double transactionFee = 0.01;
+                double fee = smallAmountValue * transactionFee;
+
+                expectedBalance = startBalance - (smallAmountValue + fee);
+                assertTrue(expectedBalance >= creditLimit);
+            }
+
+            else {
+                expectedBalance = startBalance - smallAmountValue;
+                assertTrue(expectedBalance >= 0);
+            }
+            assertEquals(expectedBalance, account.getBalance());
+
+            System.out.println(account.getClass().getName() + ": " + account.getBalance());
+
+        });
+    }
+
+    @Test
+    public void processTransactionWithdrawFailedAll() {
+        transactionProcessor.processTransaction(accounts, bigAmountValue);
+
+        accounts.forEach(account -> {
+
+            double startBalance = 1000;
+
+            assertEquals(startBalance, account.getBalance());
+        });
+    }
+
+    private ArrayList<BankAccount> getAccountCollection() {
+        ArrayList<BankAccount> collection = new ArrayList<BankAccount>();
+
+        collection.add(new DebitAccount(new ClientAccount(0, "Andreew McFly", 1000)));
+        collection.add(new CreditAccount(new ClientAccount(1, "Vasya Pupkin", 1000)));
+        collection.add(new SavingsAccount(new ClientAccount(2, "Andron Kamikadzevixh", 1000)));
+
+        return collection;
+    }
 }
